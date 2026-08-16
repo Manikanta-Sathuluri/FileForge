1ff3873f46a0a2efb2f29ed8a653366e66014ee0
Manikanta <kantamani80@gmail.com>
2026-08-16 12:28:11 +0530

    Update spinbox test for current numeric control

--- tests/test_reliable_spinbox.py
+++ tests/test_reliable_spinbox.py
@@ -1,30 +1,13 @@
-
-from PySide6.QtCore import Qt, QPoint
-from PySide6.QtGui import QMouseEvent
 from PySide6.QtWidgets import QApplication
-from app import ReliableSpinBox
+from app import NumericControl
 
 
-def test_reliable_spinbox_up_and_down_clicks():
+def test_numeric_control_up_and_down():
     app = QApplication.instance() or QApplication([])
-    box = ReliableSpinBox()
-    box.resize(140, 34)
-    box.setRange(1, 20000)
-    box.setValue(100)
 
-    right_x = box.width() - 5
-    top = QMouseEvent(
-        QMouseEvent.MouseButtonPress,
-        QPoint(right_x, 5),
-        Qt.LeftButton, Qt.LeftButton, Qt.NoModifier
-    )
-    box.mousePressEvent(top)
-    assert box.value() == 101
+    control = NumericControl(value=100)
+    control._up.click()
+    assert control.value() == 101
 
-    bottom = QMouseEvent(
-        QMouseEvent.MouseButtonPress,
-        QPoint(right_x, box.height() - 5),
-        Qt.LeftButton, Qt.LeftButton, Qt.NoModifier
-    )
-    box.mousePressEvent(bottom)
-    assert box.value() == 100
+    control._down.click()
+    assert control.value() == 100
